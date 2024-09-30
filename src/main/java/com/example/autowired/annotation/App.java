3ed45c67a594b8f835c1afdb00f266c007912239
ashishsharma <sharmaashish54@gmail.com>
2024-09-30 11:34:04 +0530

    Dummy coommit

--- src/main/java/com/example/autowired/annotation/App.java
+++ src/main/java/com/example/autowired/annotation/App.java
@@ -4,7 +4,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Employee employee = context.getBean("employee", Employee.class);
